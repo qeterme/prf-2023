2fff0e0b738314adf61c78728e092cc67f342f8d
Peter Berkecz <berkecz.peter@gmail.com>
2023-05-06 21:36:58 +0200

    refactor(frontend): put base url in env file

--- front/src/app/services/product/product.service.ts
+++ front/src/app/services/product/product.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Product } from 'src/app/model/Product';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  private baseUrl = 'http://localhost:3000/api/products';
+  private baseUrl = environment.apiUrl + '/api/products';
 
   constructor(private http: HttpClient) { }
 
