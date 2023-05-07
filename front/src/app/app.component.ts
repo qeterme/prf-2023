7307879f5bd3f761fd288ebc9b77aafea8ccff79
Peter Berkecz <berkecz.peter@gmail.com>
2023-05-07 13:42:17 +0200

    feat(frontend): added authbased thingies to menu

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { identicon } from 'minidenticons';
+import { AuthService } from './services/auth/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'front';
+  avatarSrc = 'data:image/svg+xml;utf8,' + encodeURIComponent(identicon(this.authService.getCurrentUser()?.username ?? 'anonymous'));
 
-  avatarSrc = 'data:image/svg+xml;utf8,' + encodeURIComponent(identicon(this.title));
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.avatarSrc = 'data:image/svg+xml;utf8,' + encodeURIComponent(identicon(this.authService.getCurrentUser()?.username ?? 'anonymous'));
+  }
+  
+  logout() {
+    this.authService.logout();
+    this.avatarSrc = 'data:image/svg+xml;utf8,' + encodeURIComponent(identicon(this.authService.getCurrentUser()?.username ?? 'anonymous'));
+    Swal.fire({
+      icon: 'success',
+      title: 'Logged out',
+      timer: 1500,
+      showConfirmButton: false,
+    });
+  }
+
+  isLoggedIn() {
+    this.avatarSrc = 'data:image/svg+xml;utf8,' + encodeURIComponent(identicon(this.authService.getCurrentUser()?.username ?? 'anonymous'));
+    return this.authService.getCurrentUser() != null;
+  }
+
+  isAdmin() {
+    return this.authService.isCurrentUserAdmin();
+  }
 }
